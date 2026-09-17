9d71ce6438da405a542facbc2314c3a1d4ce898b
giovannarcha <giovannarcha10@gmail.com>
2026-09-16 21:21:03 -0300

    feat: adiciona campos tipo_acervo e categoria ao model Livro

--- acervo/models.py
+++ acervo/models.py
@@ -1,10 +1,30 @@
 from django.db import models
 
 class Livro(models.Model):
+    TIPO_ACERVO = [
+        ('digital', 'Digital'),
+        ('fisico', 'Físico'),
+    ]
+
+    CATEGORIA_CDD = [
+        ('000', '000 – Generalidades e Informação'),
+        ('100', '100 – Filosofia e Psicologia'),
+        ('200', '200 – Religião e Teologia'),
+        ('300', '300 – Ciências Sociais e Direito'),
+        ('400', '400 – Linguística e Idiomas'),
+        ('500', '500 – Ciências Puras (Exatas e Naturais)'),
+        ('600', '600 – Ciências Aplicadas (Tecnologia)'),
+        ('700', '700 – Artes e Recreação'),
+        ('800', '800 – Literatura'),
+        ('900', '900 – História e Geografia'),
+    ]
+
     titulo = models.CharField(max_length=200)
     autor = models.CharField(max_length=100)
     ano = models.IntegerField()
     disponivel = models.BooleanField(default=True)
-    
+    tipo_acervo = models.CharField(max_length=10, choices=TIPO_ACERVO, default='fisico')
+    categoria = models.CharField(max_length=3, choices=CATEGORIA_CDD, default='000')
+
     def __str__(self):
         return self.titulo
