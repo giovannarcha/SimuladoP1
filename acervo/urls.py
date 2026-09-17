58fa47582466d01e0c3493a0d8d2d981a8d21ece
giovannarcha <giovannarcha10@gmail.com>
2026-09-16 21:28:48 -0300

    estrutura 4

--- acervo/urls.py
+++ acervo/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-path('', views.inicio),
+    path('', views.lista_livros, name='lista'),
+    path('novo/', views.novo_livro, name='novo_livro'),
+    path('buscar/', views.buscar_livros, name='buscar'),
 ]
