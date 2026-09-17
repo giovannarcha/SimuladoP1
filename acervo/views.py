41ccd37827d2c4c9b14aaa4eba89f036e384477b
giovannarcha <giovannarcha10@gmail.com>
2026-09-16 21:26:31 -0300

    estrutura 2

--- acervo/views.py
+++ acervo/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
-def inicio(request):
-        return HttpResponse(
-            'Olá, acervo!'
-)
+def buscar_livros(request):
+    nome = request.GET.get('nome', '')
+    tipo = request.GET.get('tipo', '')
+    categoria = request.GET.get('categoria', '')
+
+    livros = Livro.objects.all()
+
+    if nome:
+        livros = livros.filter(titulo__icontains=nome)
+    if tipo:
+        livros = livros.filter(tipo_acervo=tipo)
+    if categoria:
+        livros = livros.filter(categoria=categoria)
+
+    return render(request, 'acervo/busca.html', {
+        'livros': livros,
+        'tipos': Livro.TIPO_ACERVO,
+        'categorias': Livro.CATEGORIA_CDD,
+    })
